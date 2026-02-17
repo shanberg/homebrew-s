7340bdf64daa8b26763924b4c6549165306a32d2
Stuart Hanberg <shanberg@users.noreply.github.com>
2026-02-16 21:54:58 -0500

    maxwell-carmody: point at maxwell-carmody repo

--- Formula/maxwell-carmody.rb
+++ Formula/maxwell-carmody.rb
@@ -23,8 +23,8 @@
 class MaxwellCarmody < Formula
   desc "Deploy CLI and agent for Maxwell Carmody"
   homepage "https://github.com/shanberg/home-services"
-  url "https://github.com/shanberg/home-services/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "" # fill with: curl -sL <url> | shasum -a 256
+  url "https://github.com/shanberg/home-services/archive/fce266c8029a55a049e7e545d9e1acb57008b516.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
   version "1.0.0"
 
