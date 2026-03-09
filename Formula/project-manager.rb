5c11a8af0a4e39926030521bcd92e8f082efa1e3
Stuart Hanberg <shanberg@users.noreply.github.com>
2026-03-09 13:27:50 -0400

    project-manager 0.3.0

--- Formula/project-manager.rb
+++ Formula/project-manager.rb
@@ -5,9 +5,9 @@
 class ProjectManager < Formula
   desc "CLI for PARA-style project creation with domain-based numbering"
   homepage "https://github.com/shanberg/project-manager"
-  url "https://github.com/shanberg/project-manager/releases/download/v0.2.15/project-manager-0.2.15.tar.gz"
-  sha256 "bb027187c608fe3938f446b890b03d1a1f2094f10f5844b7f0d482ee4d3e61ee"
-  version "0.2.15"
+  url "https://github.com/shanberg/project-manager/releases/download/v0.3.0/project-manager-0.3.0.tar.gz"
+  sha256 "ae2f3faeb5b58ffb70b592236e4ea7163c029710fb9f9690321c3a51e76c3ed2"
+  version "0.3.0"
   head "https://github.com/shanberg/project-manager.git", branch: "main"
 
   def install
