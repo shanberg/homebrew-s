031ab36bbda01a82406782016e2e307a490e079f
Stuart Hanberg <shanberg@users.noreply.github.com>
2026-09-09 13:16:32 -0400

    project-manager 0.32.1

--- Casks/pm.rb
+++ Casks/pm.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "pm" do
-  version "0.32.0"
-  sha256 "b501d2b1163a4a8c51d6d76d29acdf865521211454252f880e1038b145e6afd3"
+  version "0.32.1"
+  sha256 "175720f1ea7731c6713311797fcb40974457520a87221c99822c705e4305f407"
 
   url "https://github.com/shanberg/project-manager/releases/download/v#{version}/PM-v#{version}.zip"
   name "PM"
--- Formula/project-manager.rb
+++ Formula/project-manager.rb
@@ -5,9 +5,9 @@
 class ProjectManager < Formula
   desc "CLI for PARA-style project creation with domain-based numbering"
   homepage "https://github.com/shanberg/project-manager"
-  url "https://github.com/shanberg/project-manager/releases/download/v0.32.0/project-manager-0.32.0.tar.gz"
-  sha256 "9bb049d3d369e61c1ce7ed49bb16b8e9349c39978860bd21885d0d5dbd7e8e59"
-  version "0.32.0"
+  url "https://github.com/shanberg/project-manager/releases/download/v0.32.1/project-manager-0.32.1.tar.gz"
+  sha256 "569f678349c5f2dafa9807e720a22847e4c032ee45ae775e84e130f877c18fe8"
+  version "0.32.1"
   head "https://github.com/shanberg/project-manager.git", branch: "main"
 
   def install
